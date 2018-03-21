e880b35efe63cb2dfeccf88efcb81280585ca412
R.C. Long <deac.long@wsu.edu>
2018-03-21 02:13:02 -0700

    Add whoami endpoint

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
 });
+
+app.get('/whoami', (req, res) => {
+  var l = req.headers["accept-language"];
+  var s = req.headers["user-agent"];
+  var o = {
+    "ipaddress": req.ip,
+    "language": l.slice(0, l.indexOf(",")),
+    "software": s.slice(s.indexOf("(") + 1, s.indexOf(")"))
+  };
+  res.send(o);
+});
